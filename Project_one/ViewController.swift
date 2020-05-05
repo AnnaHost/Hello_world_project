904d19516afe8e76b34f60ec3068d008c45fbd76
AnnaHost <annahost1488@yandex.ru>
2020-05-05 14:12:35 +0500

    create new peoject

---
+++ Project_one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_one
+//
+//  Created by Анна Гареева on 05.05.2020.
+//  Copyright © 2020 Anna Gareeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
